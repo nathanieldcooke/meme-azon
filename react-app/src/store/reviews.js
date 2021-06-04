cbfa2bafdb90346fa28bb1f1b151b40dcae96ae6
nathanieldcooke <nathanield.cooke@gmail.com>
2021-06-04 16:00:55 -0700

    add addReviewThunk to redux store, edit meme file to use reviews state to add reviews

--- react-app/src/store/reviews.js
+++ react-app/src/store/reviews.js
@@ -1,8 +1,15 @@
 const GET_REVIEWS = 'reviews/GET_REVIEWS'
+const ADD_REVIEW = 'reviews/ADD_REVIEWS'
+
 
 const getReviews = (reviews) => ({
     type: GET_REVIEWS,
     payload: reviews
+})
+
+const addReview = (review) => ({
+    type: ADD_REVIEW,
+    payload: review
 })
 
 export const getReviewsThunk = () => async (dispatch) => {
@@ -17,11 +24,32 @@ export const getReviewsThunk = () => async (dispatch) => {
     }
 }
 
+export const addReviewThunk = (userId, memeId, body, rating) => async (dispatch) => {
+    const res = await fetch('/api/reviews/', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ userId, memeId, body, rating })
+    })
+
+    try {
+        if (!res.ok) throw res
+        const review = await res.json();
+        dispatch(addReview(review))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export default function reviews(state = {}, action) {
     const stateDup = {...state}
     switch (action.type) {
         case GET_REVIEWS:
             return action.payload;
+        case ADD_REVIEW:
+            stateDup[action.payload.id] = action.payload
+            return stateDup
         default:
             return state;
     }
