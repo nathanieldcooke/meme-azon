292b306e039511b1976b01daae196ac5421cc0ce
nathanieldcooke <nathanield.cooke@gmail.com>
2021-06-09 14:16:28 -0700

    add price, quantity, and total to purchases Model, and create seeding

--- app/models/purchase.py
+++ app/models/purchase.py
@@ -8,7 +8,10 @@ class Purchase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     memeId = db.Column(db.Integer, db.ForeignKey('memes.id'), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
     total = db.Column(db.Integer, nullable=False)
+    purchaseId = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
 
@@ -20,7 +23,10 @@ class Purchase(db.Model):
             'id': self.id,
             'userId': self.userId,
             'memeId': self.memeId,
+            'price': self.price,
+            'quantity': self.quantity,
             'total': self.total,
+            'purchaseId': self.purchase_id,
             'meme': self.meme.to_dict(),
             'user': self.user.to_dict(),
         }
--- app/seeds/purchases.py
+++ app/seeds/purchases.py
@@ -1,7 +1,7 @@
 from app.seeds import memes
 from random import randint
 from faker import Faker
-from app.models import db, Meme, User, Review, MemesInCart, Purchase
+from app.models import db, Meme, User, Review, MemesInCart, Purchase, purchase
 fake = Faker()
 
 def seed_purchases():
@@ -12,32 +12,64 @@ def seed_purchases():
     # memes_in_purchase_hist = [18, 3, 10, 9, 7]
     memes_in_purchase_hist = [
         {
-            "memeId": 18, 
-            "total": 50,
+            "memeId": 18,
+            "price": 50,
+            "quantity": 2,
+            "total": 100,
+            "purchaseId": 153941764
         },
         {
             "memeId": 3, 
-            "total": 6,
+            "price": 2,
+            "quantity": 4,
+            "total": 8,
+            "purchaseId": 153941764
         },
         {
             "memeId": 10, 
+            "price": 3,
+            "quantity": 1,
             "total": 3,
+            "purchaseId": 153941764
         },
         {
             "memeId": 9, 
-            "total": 5,
+            "price": 1,
+            "quantity": 16,
+            "total": 16,
+            "purchaseId": 153941764
         },
         {
-            "memeId": 7, 
-            "total": 8,
-        }
+            "memeId": 7,
+            "price": 8,
+            "quantity": 3, 
+            "total": 24,
+            "purchaseId": 153941764
+        },
+        {
+            "memeId": 11,
+            "price": 4,
+            "quantity": 3, 
+            "total": 14,
+            "purchaseId": 977637769
+        },
+        {
+            "memeId": 14,
+            "price": 8,
+            "quantity": 2, 
+            "total": 16,
+            "purchaseId": 977637769
+        },
     ]
 
     for meme in memes_in_purchase_hist:
         meme_in_purchase_hist = Purchase(
             userId=user.id,
             memeId=meme['memeId'],
-            total=meme['total']
+            price=meme['price'],
+            quantity=meme['quantity'],
+            total=meme['total'],
+            purchaseId=meme['purchaseId']
         )
         db.session.add(meme_in_purchase_hist)
         db.session.commit()
