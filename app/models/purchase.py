235c3c1bb3f8b2c3bfa6a54037957f43706a248f
nathanieldcooke <nathanield.cooke@gmail.com>
2021-06-09 09:51:20 -0700

    create purchases db model

---
+++ app/models/purchase.py
@@ -0,0 +1,26 @@
+import datetime
+from .db import db
+
+
+class Purchase(db.Model):
+    __tablename__ = 'purchases'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    memeId = db.Column(db.Integer, db.ForeignKey('memes.id'), nullable=False)
+    total = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
+
+    user = db.relationship('User', back_populates='purchases')
+    meme = db.relationship('Meme', back_populates='purchases')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'userId': self.userId,
+            'memeId': self.memeId,
+            'total': self.total,
+            'meme': self.meme.to_dict(),
+            'user': self.user.to_dict(),
+        }
